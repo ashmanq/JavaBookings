1b203f1c3e04eee61ce4f5c4352555e0c752cc0a
Ashir <ashirq@outlook.com>
2020-06-11 12:02:05 +0100

    Can find course by rating.

--- src/main/java/com/codeclan/example/bookings/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/bookings/controllers/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,7 +23,11 @@ public class CourseController {
 
     @GetMapping(value = "")
     public ResponseEntity<List<Course>> findCourses(
+            @RequestParam(name="rating", required = false) Integer rating
     ) {
+        if(rating != null) {
+            return new ResponseEntity<>(courseRepository.findByRating(rating), HttpStatus.OK);
+        }
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/codeclan/example/bookings/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/bookings/repositories/CourseRepository.java
@@ -3,5 +3,8 @@ package com.codeclan.example.bookings.repositories;
 import com.codeclan.example.bookings.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByRating(int rating);
 }
