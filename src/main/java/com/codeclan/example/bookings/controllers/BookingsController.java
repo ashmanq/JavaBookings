154c02861b4bcce11b6ca0c65813b6866b1e68ef
Ashir <ashirq@outlook.com>
2020-06-11 12:49:49 +0100

    Can search for booking by date

--- src/main/java/com/codeclan/example/bookings/controllers/BookingsController.java
+++ src/main/java/com/codeclan/example/bookings/controllers/BookingsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +21,12 @@ public class BookingsController {
     BookingRepository bookingRepository;
 
     @GetMapping
-    public ResponseEntity<List<Booking>> getBookings() {
+    public ResponseEntity<List<Booking>> getBookings(
+            @RequestParam(name = "date", required = false) String date
+    ) {
+        if(date != null) {
+            return new ResponseEntity<>(bookingRepository.findByDate(date), HttpStatus.OK);
+        }
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
     }
 
--- src/main/java/com/codeclan/example/bookings/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/bookings/repositories/BookingRepository.java
@@ -3,5 +3,8 @@ package com.codeclan.example.bookings.repositories;
 import com.codeclan.example.bookings.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByDate(String date);
 }
